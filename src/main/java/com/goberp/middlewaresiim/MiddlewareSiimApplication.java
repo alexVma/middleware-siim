b9f768ccd3582b6cc6c3c5cdeebfb1e93c2a8f2a
Alex V <aminga@netlife.net.ec>
2024-02-27 21:33:24 -0500

    🎉 first commit

---
+++ src/main/java/com/goberp/middlewaresiim/MiddlewareSiimApplication.java
@@ -0,0 +1,13 @@
+package com.goberp.middlewaresiim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareSiimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareSiimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goberp/middlewaresiim/MiddlewareSiimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goberp.middlewaresiim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareSiimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
